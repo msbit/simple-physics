2bf9be606dfbcd2f7acfb3ea14d770683b724b0a
Tom Sullivan <tom@msbit.com.au>
2021-06-24 13:03:59 +1000

    initial

---
+++ index.js
@@ -0,0 +1,133 @@
+globalThis.addEventListener('load', () => {
+  const canvas = document.getElementById('canvas');
+  canvas.width = 800;
+  canvas.height = 800;
+  const context = canvas.getContext('2d');
+
+  const state = {
+    position: {
+      x: canvas.width / 2,
+      y: canvas.height / 2
+    },
+    positions: [],
+    maxVelocity: 100,
+    velocity: {
+      x: 0,
+      y: 0
+    },
+    damping: 0.1,
+    accelerationForce: 30,
+    acceleration: {
+      x: 0,
+      y: 0
+    },
+    input: {
+      up: false,
+      down: false,
+      left: false,
+      right: false
+    },
+    context,
+    entityWidth: 30,
+    entityHeight: 30
+  };
+
+  mapkeys({
+    ArrowUp: (value) => state.input.down = value,
+    ArrowDown: (value) => state.input.up = value,
+    ArrowLeft: (value) => state.input.left = value,
+    ArrowRight: (value) => state.input.right = value,
+  });
+
+  let previous = performance.now();
+  const tick = (current) => {
+    const elapsed = current - previous;
+
+    update(state, elapsed);
+
+    draw(state, elapsed);
+    
+    previous = current;
+    requestAnimationFrame(tick);
+  };
+
+  requestAnimationFrame(tick);
+});
+
+const update = (state, elapsed) => {
+  state.acceleration.x = 0;
+  state.acceleration.y = 0;
+  const force = (elapsed * state.accelerationForce) / 1000;
+  if (state.input.up) {
+    state.acceleration.y += force;
+  }
+  if (state.input.down) {
+    state.acceleration.y -= force;
+  }
+  if (state.input.left) {
+    state.acceleration.x -= force;
+  }
+  if (state.input.right) {
+    state.acceleration.x += force;
+  }
+
+  state.velocity.x += state.acceleration.x;
+  state.velocity.y += state.acceleration.y;
+
+  const damping = {
+    x: -state.velocity.x,
+    y: -state.velocity.y
+  };
+  damping.x *= state.damping;
+  damping.y *= state.damping;
+
+  state.velocity.x += damping.x;
+  state.velocity.y += damping.y;
+
+  state.velocity.x = clamp(-state.maxVelocity, state.maxVelocity, state.velocity.x);
+  state.velocity.y = clamp(-state.maxVelocity, state.maxVelocity, state.velocity.y);
+
+  state.positions.unshift({...state.position});
+  state.positions.splice(400);
+
+  state.position.x += state.velocity.x;
+  state.position.y += state.velocity.y;
+
+  state.position.x = clamp(state.entityWidth / 2, state.context.canvas.width - (state.entityWidth / 2), state.position.x);
+  state.position.y = clamp(state.entityHeight / 2, state.context.canvas.height - (state.entityHeight / 2), state.position.y);
+};
+
+const draw = (state, elapsed) => {
+  state.context.fillStyle = 'whitesmoke';
+  state.context.fillRect(0, 0, state.context.canvas.width, state.context.canvas.height);
+
+  state.context.strokeStyle = 'cornflowerblue';
+  state.context.beginPath();
+  state.context.moveTo(state.position.x, state.position.y);
+  for(const position of state.positions) {
+    state.context.lineTo(position.x, position.y);
+  }
+  state.context.stroke();
+
+  state.context.fillStyle = 'orangered';
+  state.context.fillRect(state.position.x - (state.entityWidth / 2), state.position.y - (state.entityHeight / 2), state.entityWidth, state.entityHeight);
+
+  state.context.fillStyle = 'slategrey';
+  state.context.font = '20px sans-serif';
+  state.context.fillText((1000 / elapsed).toFixed(2), 0, 20);
+};
+
+const clamp = (min, max, value) => Math.max(min, Math.min(max, value));
+
+const mapkeys = (mappings, target = globalThis) => {
+  target.addEventListener('keydown', (event) => {
+    if (!mappings.hasOwnProperty(event.key)) { return; }
+
+    mappings[event.key](true);
+  });
+  target.addEventListener('keyup', (event) => {
+    if (!mappings.hasOwnProperty(event.key)) { return; }
+
+    mappings[event.key](false);
+  });
+};
